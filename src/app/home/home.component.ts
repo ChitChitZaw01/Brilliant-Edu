442609e10a5ea0fe6b1f94402ef0224d22b340ce
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-02-10 00:54:03 +0700

    Updated codes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCard],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

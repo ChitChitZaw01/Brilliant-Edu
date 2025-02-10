8ef6b0be54b9f09b3a61c711996c3b8d94bd861d
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-02-10 16:47:30 +0700

    updated images and home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCard } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   imports: [MatCard,
+    MatToolbarModule,
     FooterComponent
   ],
   templateUrl: './home.component.html',

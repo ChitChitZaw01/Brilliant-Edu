f7d03902b42efd226a96224455fcf5eaa358ebc3
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-02-11 01:38:33 +0700

    Updated Home, AboutUs and courses pages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MatCard } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MatCard,
+  imports: [
     MatToolbarModule,
     FooterComponent
   ],

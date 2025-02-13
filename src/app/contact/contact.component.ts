8136204eb6b73be685346999901c240f46a6bf7d
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-02-13 21:30:34 +0700

    Updated Contact Us page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,36 +1,64 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { CommonModule } from '@angular/common'; 
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [FormsModule,
+  imports: [MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    CommonModule,
     FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  constructor() {}
+  model = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  isSubmitted = false;
 
   onSubmit() {
-    // You can replace the 'url' with your FormSubmit service URL.
-    const formData = new FormData();
-    formData.append('name', 'Name goes here');
-    formData.append('email', 'Email goes here');
-    formData.append('message', 'Message goes here');
-    
-    fetch('https://formsubmit.co/ajax/chitchitzaw.as16@gmail.com', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      // Handle success, show message to user, etc.
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      // Handle error, show error message, etc.
+    const formData = {
+      name: this.model.name,
+      email: this.model.email,
+      message: this.model.message,
+    };
+
+    this.submitForm(formData);
+  }
+
+  submitForm(formData: any) {
+    const url = 'https://formsubmit.co/ajax/chitchitzaw.as16@gmail.com';
+    const headers = new Headers({
+      'Content-Type': 'application/json',
     });
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: headers,
+    })
+      .then((response) => {
+        if (response.ok) {
+          this.isSubmitted = true;
+          this.resetForm();
+        } else {
+          console.error('Error submitting form:', response);
+        }
+      })
+      .catch((error) => {
+        console.error('Error submitting form:', error);
+      });
+  }
+
+  resetForm() {
+    this.model = { name: '', email: '', message: '' };
   }
   }

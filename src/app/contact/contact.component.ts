e72a297845499d04c40299ebe13d8223bc1dc620
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-02-13 17:47:35 +0700

    Updated Contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [FooterComponent],
+  imports: [FormsModule,
+    FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  constructor() {}
 
-}
+  onSubmit() {
+    // You can replace the 'url' with your FormSubmit service URL.
+    const formData = new FormData();
+    formData.append('name', 'Name goes here');
+    formData.append('email', 'Email goes here');
+    formData.append('message', 'Message goes here');
+    
+    fetch('https://formsubmit.co/ajax/chitchitzaw.as16@gmail.com', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      // Handle success, show message to user, etc.
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      // Handle error, show error message, etc.
+    });
+  }
+  }

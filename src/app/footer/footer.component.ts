5b63d6cc9d4f48ea1b62da2f839a6ef122de96cb
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-02-13 23:49:40 +0700

    Updated for responsive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
